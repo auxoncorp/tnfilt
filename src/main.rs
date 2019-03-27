61120e2ff58cdbd257ad52ce9d2ed4ac52ff631c
Russell Mull <russell@auxon.io>
2019-03-27 09:31:25 -0700

    Initial revision

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+extern crate nom;
+use nom::*;
+use std::io::{self, BufRead};
+
+#[derive(Debug)]
+enum Bit {
+    B0,
+    B1,
+}
+
+impl Bit {
+    fn val(&self) -> u32 {
+        match self {
+            Bit::B0 => 0,
+            Bit::B1 => 1,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Unsigned {
+    UTerm,
+    UInt(Box<Unsigned>, Bit),
+}
+
+impl Unsigned {
+    fn from_tuple((us, b): (Unsigned, Bit)) -> Unsigned {
+        Unsigned::UInt(Box::new(us), b)
+    }
+
+    fn as_u32(&self) -> u32 {
+        match self {
+            Unsigned::UTerm => 0,
+            Unsigned::UInt(inner, bit) => (inner.as_u32() << 1) | bit.val(),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Term {
+    Unsigned(Unsigned),
+    Char(char),
+}
+
+named!(bit<&str, Bit>,
+       preceded!(
+           tag!("typenum::B"),
+           alt!(
+               value!(Bit::B0, tag!("0")) | value!(Bit::B1, tag!("1"))
+           )
+       )
+);
+
+named!(unsigned<&str, Unsigned>,
+       alt!(
+           value!(Unsigned::UTerm, tag!("typenum::UTerm")) |
+           delimited!(
+               tag!("typenum::UInt<"),
+               map!(
+                   separated_pair!(unsigned, tag!(", "), bit),
+                   Unsigned::from_tuple
+               ),
+               tag!(">")
+           )
+       )
+);
+
+named!(line<&str, Vec<Term> >,
+       many0!(
+           alt_complete!(
+               map!(unsigned, Term::Unsigned) | map!(anychar, Term::Char)
+           )
+       )
+);
+
+fn main() {
+    for l in io::stdin().lock().lines().map(|l| l.unwrap()) {
+        for term in line(&l).unwrap().1 {
+            match term {
+                Term::Char(c) => print!("{}", c),
+                Term::Unsigned(u) => print!("U{}", u.as_u32()),
+            }
+        }
+        println!("");
+    }
+}
